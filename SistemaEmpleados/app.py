5421a528c9e245bc5217a2a9da80abc4416c89c5
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2023-05-25 09:16:38 -0500

    Update app.py

--- SistemaEmpleados/app.py
+++ SistemaEmpleados/app.py
@@ -1,18 +1,26 @@
 from flask import Flask
-from flask import render_template, redirect, request
-
+from flask import render_template, redirect, request, url_for
+from flask import send_from_directory
 from flaskext.mysql import MySQL
 from datetime import datetime
-
+import os
+ 
 app = Flask(__name__)
 
 mysql = MySQL()
+
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema'
 mysql.init_app(app)
 
+CARPETA= os.path.join('uploads')
+app.config['CARPETA']= CARPETA
+
+@app.route('/uploads/<nombreFoto>')
+def uploads(nombreFoto):
+    return send_from_directory(app.config['CARPETA'],nombreFoto)
 
 @app.route('/')
 def index():
@@ -26,11 +34,14 @@ def index():
     conn.commit()  
     return render_template('empleados/index.html', empleados = empleados )
 
-
 @app.route('/destroy/<int:id>')
 def destroy(id):
     conn=mysql.connect()
     cursor=conn.cursor()
+    cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+    fila=cursor.fetchall()
+    os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        
     cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
     conn.commit()
     return redirect('/')
@@ -55,14 +66,49 @@ def storage():
     sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
     
     datos= (_nombre, _correo, _foto.filename)
-    
+  
     conn = mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql, datos)
     conn.commit()
     return render_template('empleados/index.html')
 
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    id=request.form['txtID']
+    _foto=request.files['txtFoto']
+    sql = "UPDATE empleados SET nombre= %s, correo=%s WHERE id=%s;"   
+    datos= (_nombre, _correo, id)
+    conn = mysql.connect()
+    cursor=conn.cursor()
+    now=datetime.now()
+    tiempo=now.strftime("%Y%M%H%S")
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads"+nuevoNombreFoto)
+        cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+        fila=cursor.fetchall()
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s", (nuevoNombreFoto,id))
+        conn.commit()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/')    
+
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    conn = mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id=%s",(id))
+    empleados=cursor.fetchall()
+    conn.commit()
+    print(empleados)   
+    return render_template('empleados/edit.html',empleados=empleados)
+
+
    
-    
 if __name__ == '__main__':
     app.run(debug=True)
